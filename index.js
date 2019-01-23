7a95cce8fbf4072d2dcba5fa7bff6cd9dc9b091c
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-01-23 13:49:09 -0600

    lookin good?

---
+++ index.js
@@ -0,0 +1,32 @@
+var nodemailer = require('nodemailer');
+var fs = require('fs');
+var contents = fs.readFileSync('secrets.json')
+var jsonContent = JSON.parse(contents);
+
+var email = jsonContent.'email';
+var passw = jsonContent.'password';
+var number = jsonContent.'number';
+
+var transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+        user: email,
+        pass: passw
+    }
+});
+
+function send(){
+  const mailOptions = {
+    from: email, // sender address
+    to: number, // list of receivers
+    subject: '', // Subject line
+    html: '<p>hey mom</p>'// plain text body
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+     if(err)
+       console.log(err)
+     else
+       console.log(info);
+  });
+}
