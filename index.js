036a7d37fe0473418addd10c897ea3ba0ca85754
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-01-29 15:34:52 -0600

    now for sure

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var sendSMS = require('./js/sendSMS');
 var emojify = require('./js/emojify');
 
 app.use(express.urlencoded());
-app.use('/mom', express.static('public'));
+app.use('/public', express.static(path.join(__dirname + 'public')));
 app.get('/mom', function(req,res){
   res.sendFile(__dirname + '/public/index.html');
 });
