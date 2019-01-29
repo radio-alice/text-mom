a40a818bcebb3820b52c1d8679fbfd3c1153e51d
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-01-29 15:38:01 -0600

    jk this is the fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var sendSMS = require('./js/sendSMS');
 var emojify = require('./js/emojify');
 
 app.use(express.urlencoded());
-app.use('/public', express.static(path.join(__dirname + 'public')));
+app.use('/', express.static('/public'));
 app.get('/mom', function(req,res){
   res.sendFile(__dirname + '/public/index.html');
 });
