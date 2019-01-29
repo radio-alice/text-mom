d3bd4393ebff44c092cdb6b4101b610d30c6320f
Zach Sherman <zachsherman@cs.uchicago.edu>
2019-01-29 00:22:19 -0600

    hmm?

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var emojify = require('./js/emojify');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static('/public'));
-app.get('/', function(req,res){
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req,res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 app.post('/submit', function(req, res){
    const message = req.body.message;
    const emojMessage = emojify.clean(message);
